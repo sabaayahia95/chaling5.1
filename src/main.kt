d37ef8b7fb45b3b4f04ddf15946141162033a269
sabaa yahia <sabaa yahia4@gmail.com>
2020-09-05 16:51:07 +0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,19 @@
+
+fun main() {
+    var r= Sumation(5, 7);
+    r.sum()
+    println("result="+ "  "+(5+7))
+}
+
+class Sumation (num1:Int , num2:Int) {
+    var numo1 =0;
+    var numt2= 0;
+
+ fun  sum(){
+
+ }
+
+}
+
+
+
